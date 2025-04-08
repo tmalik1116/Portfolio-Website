bd7ddb217e02ed30439ef5fb7ce156e19202cb7e
Talha Malik <tmalik2004@gmail.com>
2025-04-08 01:05:30 -0400

    added email icon

--- personal-website/src/NavBar.jsx
+++ personal-website/src/NavBar.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import githubLogo from './assets/Copy of GitHub Logo.png';
-import linkedinLogo from './assets/LinkedIn Logo.png';
-import linkedinLogoTrans from './assets/LinkedIn Logo Transparent.png';
+import linkedinLogo from './assets/LinkedIn Logo Transparent.png';
+import email from './assets/email icon.png';
 
 export default function NavBar(props) {
-
     return (
-        <div className="fixed top-0 left-0 w-full h-11 bg-gray-900 pt-2 shadow-lg shadow-purple-900 z-50">
-            <div className="flex pl-0 ml-0">
-                <h3 className="w-1/5 left-1.5 font-bold ml-0 mr-auto cursor-pointer hover:text-purple-400 transition-all 150ms ease-in-out">Talha Malik</h3>
-                <h4 className="w-1/5 ml-2 mr-auto cursor-pointer hover:text-purple-400 transition-all 150ms ease-in-out">Projects</h4>
-                <h4 className="w-1/5 ml-2 mr-auto cursor-pointer hover:text-purple-400 transition-all 150ms ease-in-out">Experience</h4>
-                <h4 className="w-1/5 ml-2 mr-auto cursor-pointer hover:text-purple-400 transition-all 150ms ease-in-out">Contact</h4>
-                <div className="flex w-1/5">
-                    <a href="https://linkedin.com/in/talha-malik-software-developer">
-                        <img src={linkedinLogoTrans} className="w-7 h-7 p-0 mr-0 ml-40 rounded-full bg-black cursor-pointer hover:bg-blue-700 hover:h-8 hover:w-8 hover:-mt-0.5 transition-all 100ms ease-in-out absolute"></img>
+        <div className="fixed top-0 left-0 w-full h-max bg-gray-900 pt-2 shadow-lg shadow-purple-900 z-50">
+            <div className="flex justify-between items-center px-4 mb-2">
+                <h3 className="font-bold cursor-pointer hover:text-purple-400 transition-all 150ms ease-in-out">Talha Malik</h3>
+                
+                <div className="flex space-x-8 flex-grow justify-center">
+                    <h4 className="cursor-pointer hover:text-purple-400 transition-all 150ms ease-in-out">Projects</h4>
+                    <h4 className="cursor-pointer hover:text-purple-400 transition-all 150ms ease-in-out">Experience</h4>
+                    <h4 className="cursor-pointer hover:text-purple-400 transition-all 150ms ease-in-out">Contact</h4>
+                </div>
+                
+                <div className="flex space-x-4 items-center mt-0.5">
+                    <a href="mailto:tmalik2004@gmail.com?subject=Let's%20Connect!&body=Hello%20Talha%2C%0A%0A" className="relative w-8 h-8">
+                        <img 
+                            src={email} 
+                            className="absolute inset-0 w-7 h-7 rounded-full bg-black cursor-pointer hover:bg-red-700 hover:h-8 hover:w-8 hover:-mt-0.5 hover:-ml-0.5 transition-all 100ms ease-in-out"
+                        />
+                    </a>
+
+                    <a href="https://linkedin.com/in/talha-malik-software-developer" className="relative w-8 h-8">
+                        <img 
+                            src={linkedinLogo} 
+                            className="absolute inset-0 w-7 h-7 rounded-full bg-black cursor-pointer hover:bg-blue-700 hover:h-8 hover:w-8 hover:-mt-0.5 hover:-ml-0.5 transition-all 100ms ease-in-out"
+                        />
                     </a>
                     
-                    <a href="https://github.com/tmalik1116">
-                        <img src={githubLogo} className="w-7 h-7 pt-0 mr-2 ml-50 rounded-full bg-white cursor-pointer hover:bg-indigo-400 hover:h-8 hover:w-8 hover:-mt-0.5 transition-all 100ms ease-in-out absolute"></img>
+                    <a href="https://github.com/tmalik1116" className="relative w-8 h-8">
+                        <img 
+                            src={githubLogo} 
+                            className="absolute inset-0 w-7 h-7 rounded-full bg-white cursor-pointer hover:bg-indigo-400 hover:h-8 hover:w-8 hover:-mt-0.5 hover:-ml-0.5 transition-all 100ms ease-in-out"
+                        />
                     </a>
                 </div>
             </div>
