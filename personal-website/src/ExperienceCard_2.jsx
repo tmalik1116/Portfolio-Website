cbf7192697212a1cfac66eb386619af65b3e5109
Talha Malik <tmalik2004@gmail.com>
2025-04-27 16:57:35 -0400

    fixed some formatting for mobile

--- personal-website/src/ExperienceCard_2.jsx
+++ personal-website/src/ExperienceCard_2.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
 
-export default function ExperienceCard(props) {
-  return (
-    <div
-        className="flex flex-col sm:max-w-[95%] md:max-w-[55%] mx-auto my-3 bg-gray-900 rounded-2xl p-3 select-none">
-        <div className="card w-full flex flex-col md:flex-row justify-between items-center gap-2">
-            {/* Left Column: 60% on md and up */}
-            <div className="md:w-full pl-3 pt-0">
-                <h2 className="font-bold">{props.name}</h2>
-                <h3 className="font-light md:mb-3">{props.company} | {props.timeframe}</h3>
-                <ul className="list-disc ml-5 break-words">
-                    <li className="text-left mb-2">{props.point1}</li>
-                    <li className="text-left">{props.point2}</li>
-                </ul>
-            </div>
+export default function ExperienceCard_2(props) {
+    return (
+        <div className="flex flex-col w-full md:max-w-[55%] mx-auto my-4 bg-gray-900 rounded-2xl p-3 select-none">
+            <div className="card w-full flex flex-col md:flex-row justify-between items-center gap-4">
+                
+                {/* Left side: text */}
+                <div className="w-full md:w-3/5 pl-3">
+                    <h2 className="font-bold text-white text-xl mb-1">{props.name}</h2>
+                    <h3 className="font-light text-gray-400 mb-3">{props.company} | {props.timeframe}</h3>
+                    <ul className="list-disc ml-5 text-gray-300 space-y-2">
+                        <li>{props.point1}</li>
+                        <li>{props.point2}</li>
+                    </ul>
+                </div>
 
-            {/* Right Column: 40% on md and up */}
-            <div className="bg-white w-80 rounded-lg flex items-center justify-center p-3">
-                <img
-                    src={props.image}
-                    alt={props.name}
-                    className="object-contain"
-                />
+                {/* Right side: IMAGE AREA */}
+
+                {/* MOBILE: full width rectangular banner */}
+                <div className="w-full flex justify-center mb-0 md:hidden">
+                    <img 
+                        src={props.imageMobile}
+                        alt={props.name}
+                        className="object-contain w-full h-auto rounded-lg bg-white p-3"
+                    />
+                </div>
+
+                {/* DESKTOP: classic square container */}
+                <div className="hidden md:flex bg-white rounded-lg p-3 w-64 h-64 items-center justify-end">
+                    <img 
+                        src={props.imageDesktop}
+                        alt={props.name}
+                        className="object-contain h-full w-full"
+                    />
+                </div>
+
             </div>
-        </div>  
-    </div>
-  );
+        </div>
+    );
 }
--- personal-website/src/ExperienceMenu.jsx
+++ personal-website/src/ExperienceMenu.jsx
@@ -1,49 +1,56 @@
 import React from 'react';
 import ExperienceCard_2 from './ExperienceCard_2';
 import iGEM_2 from './assets/iGEM logo small.jpg';
+import iGEM_1 from './assets/iGEM logo.png';
 import Chandos_2 from './assets/Chandos logo small.webp';
-import career_arrow from './assets/career_arrow.png';
+import Chandos_1 from './assets/Chandos logo.png';
 import kettleby_1 from './assets/kettleby_logo_upscayl_5x_digital-art-4x.png';
+import kettleby_2 from './assets/kettleby_logo_2.png';
+import career_arrow from './assets/career_arrow.png';
 
 export default function ExperienceMenu() {
     return (
         <div className="flex flex-col w-full md:w-max h-full mt-20 px-4 justify-around">
-            {/* w-full on mobile, w-max on large screens */}
             <h1 className="text-center font-semibold text-2xl mb-6">Experience</h1>
 
             <div className="flex flex-col pt-5">
                 
+                {/* Card 1 */}
                 <ExperienceCard_2
                     name="Modelling & Software General Member"
                     company="iGEM Guelph"
                     timeframe="March 2025 - Present"
                     point1="Contributing to technical efforts of the University’s team for an international biological research competition"
                     point2="Responsibilities will include software modelling, data analysis, and collaborative software development" 
-                    image={iGEM_2}
+                    imageMobile={iGEM_1}
+                    imageDesktop={iGEM_2}
                 />
                 
                 <img className="h-30 object-contain my-5" src={career_arrow} alt="Arrow" />
 
+                {/* Card 2 */}
                 <ExperienceCard_2
                     name="Student Project Coordinator (Co-op)"
                     company="Chandos Construction"
                     timeframe="May 2024 - December 2024"
                     point1="Developed a wide variety of Python and VBA scripts to automate repetitive tasks, improving efficiency of common tasks by up to 400% between cross-functional teams"
                     point2="Gained vital skills in data analysis, process automation, project management, B2B communication, and database management using ViewPoint FieldView" 
-                    image={Chandos_2}
+                    imageMobile={Chandos_1}
+                    imageDesktop={Chandos_2}
                 />
 
                 <img className="h-30 object-contain my-5" src={career_arrow} alt="Arrow" />
 
+                {/* Card 3 */}
                 <ExperienceCard_2
                     name="Day Counsellor"
                     company="Kettleby Valley Camp"
                     timeframe="June 2023 - August 2023"
                     point1="Gained valuable team experience working alongside peers, and was often tasked with leadership roles which entailed managing a number of staff members"
                     point2="Gained valuable teaching experience as well as the ability to supervise and lead a group of individuals" 
-                    image={kettleby_1}
+                    imageMobile={kettleby_2}
+                    imageDesktop={kettleby_1}
                 />
-
             </div>
         </div>
     )
